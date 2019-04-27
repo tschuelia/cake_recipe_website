39e0d904b5a9420adf665f0389f6298fdd935781
tschuelia <julaschmid@googlemail.com>
2019-04-27 08:47:59 +0200

    beautify website with bootstrap, implement first data model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    ingredients = models.TextField()
+    directions = models.TextField()
+    prep_time = models.CharField(max_length=100)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
+
+    def show_categories(self):
+        return ', '.join([cat.title for cat in self.categories.all()])
+
+    def print_ingredients(self):
+        ing = self.ingredients.split(',')
+        list = []
+        for i in ing:
+            list.append(i)
+        return list
--- recipes/views.py
+++ recipes/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-recipes = [
-    {
-        'categorie': 'Torten',
-        'title' : 'Käse-Sahne-Torte',
-        'ingredients': 'Sahne, Quark',
-        'making': 'Zeug zusammen rühren, backen',
-        'time': '3 Stunden'
-    },
-    {
-        'categorie': 'Kuchen',
-        'title' : 'Apple Pie',
-        'ingredients': 'Äpfel',
-        'making': 'Zeug zusammen rühren, backen',
-        'time': '1 Stunde'
-    }
-]
+from .models import Recipe, Category
 
 def home(request):
     context = {
-        'recipes': recipes
+        'recipes': Recipe.objects.all()
     }
     return render(request, 'recipes/home.html', context)
 
