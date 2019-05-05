b42b1c4c1aed91448e19a6a2db69970cad571eec
tschuelia <julaschmid@googlemail.com>
2019-05-05 18:10:07 +0200

    get gitignore working

--- recipes/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Food, Ingredient, Category, Recipe
-
-admin.site.register(Food)
-admin.site.register(Ingredient)
-admin.site.register(Category)
-admin.site.register(Recipe)
